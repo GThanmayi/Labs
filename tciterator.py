5a2bae66794d8c343c2da6be0ede4c23b04b1773
Gollavilli Thanmayi <140052441+GThanmayi@users.noreply.github.com>
2026-01-14 17:08:33 +0530

    Add files via upload

---
+++ tciterator.py
@@ -0,0 +1,26 @@
+#iteratot used to iterate one
+data=[1,2,3]
+i=iter(data)
+print(next(i))
+print(next(i))
+
+for x in [10,20,30]:
+    print(x)
+
+#custom iterate
+class count:
+    def __init__(self,limit):
+        self.limit=limit
+        self.current=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current<=self.limit:
+            val=self.current
+            self.current+=1
+            return val
+        else:
+            raise StopIteration
+obj=count(3)
+for num in obj:
+    print(num)
