b384604c62a5a1fb95b4e4f03e654ff0d8b2296e
Gollavilli Thanmayi <140052441+GThanmayi@users.noreply.github.com>
2026-01-14 17:14:47 +0530

    Day 2 assignment

--- Q1.2.py
+++ Q1.2.py
@@ -1,3 +1,11 @@
-numbers=range(1,21)
-evennumbers=list(filter(lambda x:x%2==0, numbers))
-print(evennumbers)
+def fib(n):
+    a,b=0,1
+    while n>0:
+        c=a+b
+        yield a
+        a=b
+        b=c
+        n=n-1
+
+for num in fib(10):
+    print(num)
--- Q1.py
+++ Q1.py
@@ -1,30 +1,20 @@
-import time
+class Count:
+    def __init__(self,limit):
+        self.limit=limit
+        self.current=1
 
-# Decorator to measure and print execution time
-def execution_time(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(f"Function '{func.__name__}' execution time: {end - start:.6f} seconds")
-        return result
-    return wrapper
+    def __iter__(self):
+        return self
 
-# Function to write numbers 1–100 into a file
-def write_numbers_to_file(filename):
-    with open(filename, "w") as file:
-        for num in range(1, 101):
-            file.write(str(num) + "\n")
+    def __next__(self):
+        if self.current<=self.limit:
+            val=self.current
+            self.current+=1
+            return val
 
-# Recursive factorial function decorated to show execution time
-@execution_time
-def factorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+        else:
+            raise StopIteration
 
-# Example usage
-if __name__ == "__main__":
-    write_numbers_to_file("numbers.txt")
-    print("Factorial of 10:", factorial(10))
+obj=Count(int(input()))
+for num in obj:
+    print(num)
