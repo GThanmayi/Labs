f6ffd389ed6d7faa9491f0e03fdab257e106d66a
Gollavilli Thanmayi <140052441+GThanmayi@users.noreply.github.com>
2026-01-16 14:12:44 +0530

    Delete file_wr.py

--- file_wr.py
+++
@@ -1,9 +0,0 @@
-# file_writer.py
-
-def write_numbers_to_file(filename):
-    """
-    Writes the numbers 1–100 into a text file named filename.
-    """
-    with open(filename, "w") as file:
-        for num in range(1, 101):
-            file.write(str(num) + "\n")
