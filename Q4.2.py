d6392c2e1ece714f1902670988e49af737e25ca7
Gollavilli Thanmayi <140052441+GThanmayi@users.noreply.github.com>
2026-01-16 14:18:17 +0530

    Delete Q4.2.py

--- Q4.2.py
+++
@@ -1,8 +0,0 @@
-class Bank_Account:
-    def __init__(self,account_num,balance):
-        self.account_num=account_num
-        self.balance=balance
-
-s1=Bank_Account("jam",100)
-
-print(s1.account_num,s1.balance)
