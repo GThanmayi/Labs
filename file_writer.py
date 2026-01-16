83b7da0da0069eb697a5b5ec57246088ab893b7b
Gollavilli Thanmayi <140052441+GThanmayi@users.noreply.github.com>
2026-01-16 14:12:06 +0530

    Delete file_writer.py

--- file_writer.py
+++
@@ -1,6 +0,0 @@
-def write_numbers_to_file(filename):
-    # Open the file in write mode
-    with open(filename, "w") as file:
-        # Write numbers 1 through 10, each on a new line
-        for num in range(1, 11):
-            file.write(str(num) + "\n")
